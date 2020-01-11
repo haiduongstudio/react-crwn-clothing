b1f84a095e06cdc52d9f14805f116b8ca594a6ac
Hai Nguyen Tien <haitin4bk@gmail.com>
2020-01-11 16:42:59 +0700

    Created user and cart sagas for sign in, sign up and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import {
+  googleSignInStart,
+  emailSignInStart
+} from '../../redux/user/user.actions';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -19,15 +23,10 @@ class SignIn extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    const { emailSignInStart } = this.props;
     const { email, password } = this.state;
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
-    } catch (error) {
-      console.log(error);
-    }
+    emailSignInStart(email, password);
   };
 
   handleChange = event => {
@@ -37,6 +36,7 @@ class SignIn extends Component {
   };
 
   render() {
+    const { googleSignInStart } = this.props;
     return (
       <SignInContainer>
         <SignInTitle>I already have an account</SignInTitle>
@@ -62,7 +62,11 @@ class SignIn extends Component {
 
           <ButtonsBarContainer>
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={googleSignInStart}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </ButtonsBarContainer>
@@ -72,4 +76,10 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) =>
+    dispatch(emailSignInStart({ email, password }))
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,38 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionsSnapshortToMap
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from './shop.actions';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshort = yield collectionRef.get();
+    const collectionMap = yield call(
+      convertCollectionsSnapshortToMap,
+      snapshort
+    );
+    yield put(fetchCollectionsSuccess(collectionMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionStart)]);
+}
